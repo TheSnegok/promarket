9dc46de9677ab38b89bfee3782388f4504d64ebd
Max <lord180499@gmail.com>
2022-02-14 13:29:55 +0200

    create header

--- src/Components/Main.tsx
+++ src/Components/Main.tsx
@@ -1,10 +1,37 @@
 import React from 'react'
 import s from '../Styles/blocks/header.module.sass';
+import logo from '../Image/logo.svg';
+import mark from '../Image/Subtract.svg';
 
 const Main = () => {
-  return (
-    <div className={s.main}>Main</div>
-  )
+	return (
+		<div className="container">
+			<header className={s.header}>
+				<div className={s.headerLogo}>
+					<img src={logo} alt="logo" className={s.logo} />
+				</div>
+				<div className={s.headerLocation}>
+					<img src={mark} alt="mark" className={s.mark} />
+					<div className={s.regions}>Москва</div>
+				</div>
+				<div className={s.headerPhone}>
+					<div className={s.number}>
+						8 495 018-32-10
+					</div>
+					<div className={s.popup}>
+					</div>
+					<div className={s.phoneCall}>Заказать звонок</div>
+				</div>
+				<div className={s.headerMenu}>
+					<div className={s.menuitem}>Оплата</div>
+					<div className={s.menuitem}>Доставка</div>
+					<div className={s.menuitem}>Поставщикам</div>
+					<div className={s.menuitem}>Статьи</div>
+					<div className={s.menuitem}>Контакты</div>
+				</div>
+			</header>
+		</div>
+	)
 }
 
 export default Main;
