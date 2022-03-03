78a169e3a455ca50ee97f8bee690c3ee8bf9b70f
Max <lord180499@gmail.com>
2022-03-03 12:06:15 +0200

    made seo block

--- src/Components/Main.tsx
+++ src/Components/Main.tsx
@@ -48,6 +48,8 @@ import article1 from '../Image/article/image 12.png'
 import article2 from '../Image/article/image 13.png'
 import article3 from '../Image/article/image 14.png'
 import article4 from '../Image/article/image 15.png'
+// img for block seo
+import seoImg from '../Image/seoBlock.png'
 
 const Main = () => {
 	return (
@@ -799,6 +801,25 @@ const Main = () => {
 					</article>
 					{/* <div className={s.wrapperArrowRight}></div>
 						<div className={s.wrapperArrowLeft}></div> */}
+				</div>
+			</section>
+			<section className={s.seo}>
+				<div className={s.seoBlock}>
+					<h2 className={s.seoBlockHeader}>Блок SEO-текста про магазин</h2>
+					<span className={s.seoBlockText}>GG Promarket — крупный ритейлер сантехнического оборудования, плитки <br/>
+					и аксессуаров. Мы нацелены на то, чтобы сделать процесс выбора <br/>
+					и покупки сантехники максимально простым и интуитивно понятным. <br/>
+					При создании и обновлении сайта santehnika-online.ru мы продумали <br/>
+					логичную структуру каталога, организовали систему поиска, снабдили <br/>
+					карточки товаров подробными описаниями и характеристиками. <br/>
+					<br/>
+					Выбирать оборудование в нашем магазине легко даже тем, кто впервые<br/>
+					имеет дело с сантехникой. А уточнить детали и получить информацию<br/> 
+					о совместимости товаров круглосуточно помогут опытные консультанты<br/> 
+					нашего контакт-центра. С нами вы полюбите выбирать!</span>
+				</div>
+				<div className={s.seoImage}>
+					<img src={seoImg} alt="seo img" />
 				</div>
 			</section>
 		</div>
