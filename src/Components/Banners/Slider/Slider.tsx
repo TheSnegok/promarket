42b0a5cc2b2ef679b419b0008cc79c0472f6968a
Max <lord180499@gmail.com>
2022-04-07 18:45:57 +0300

    mading css for slader and fix padding bot

--- src/Components/Banners/Slider/Slider.tsx
+++ src/Components/Banners/Slider/Slider.tsx
@@ -13,20 +13,12 @@ const Slider: FC = () => {
                     уже в наличии
                 </div>
                 <button className={s.slideButton}>ПОДРОБНЕЕ</button>
-                <div className={s.dots}>
+                <div className={s.slideDots}>
                     <div className={s.firstDot}></div>
                     <div className={s.secondDot}></div>
                     <div className={s.thirdDot}></div>
                     <div className={s.fourthDot}></div>
                     <div className={s.fivethDot}></div>
-                </div>
-                <div className={s.arrowLeft}>
-                    <div className={s.arrowLeftLineOne}></div>
-                    <div className={s.arrowLeftLineTwo}></div>
-                </div>
-                <div className={s.arrowRight}>
-                    <div className={s.arrowRightLineOne}></div>
-                    <div className={s.arrowRightLineTwo}></div>
                 </div>
             </div>
             <div className={s.slideSecond}>
@@ -38,20 +30,12 @@ const Slider: FC = () => {
                     уже в наличии
                 </div>
                 <button className={s.slideButton}>ПОДРОБНЕЕ</button>
-                <div className={s.dots}>
+                <div className={s.slideDots}>
                     <div className={s.firstDot}></div>
                     <div className={s.secondDot}></div>
                     <div className={s.thirdDot}></div>
                     <div className={s.fourthDot}></div>
                     <div className={s.fivethDot}></div>
-                </div>
-                <div className={s.arrowLeft}>
-                    <div className={s.arrowLeftLineOne}></div>
-                    <div className={s.arrowLeftLineTwo}></div>
-                </div>
-                <div className={s.arrowRight}>
-                    <div className={s.arrowRightLineOne}></div>
-                    <div className={s.arrowRightLineTwo}></div>
                 </div>
             </div>
             <div className={s.slideThird}>
@@ -63,71 +47,51 @@ const Slider: FC = () => {
                     уже в наличии
                 </div>
                 <button className={s.slideButton}>ПОДРОБНЕЕ</button>
-                <div className={s.dots}>
+                <div className={s.slideDots}>
                     <div className={s.firstDot}></div>
                     <div className={s.secondDot}></div>
                     <div className={s.thirdDot}></div>
                     <div className={s.fourthDot}></div>
                     <div className={s.fivethDot}></div>
-                </div>
-                <div className={s.arrowLeft}>
-                    <div className={s.arrowLeftLineOne}></div>
-                    <div className={s.arrowLeftLineTwo}></div>
-                </div>
-                <div className={s.arrowRight}>
-                    <div className={s.arrowRightLineOne}></div>
-                    <div className={s.arrowRightLineTwo}></div>
                 </div>
             </div>
             <div className={s.slideFourth}>
-                <div className={s.sliderHeader}>
+                <div className={s.slideHeader}>
                     СМЕСИТЕЛИ<br />
                     RAIBER
                 </div>
-                <div className={s.sliderSubheader}>
+                <div className={s.slideSubheader}>
                     уже в наличии
                 </div>
-                <button className={s.sliderButton}>ПОДРОБНЕЕ</button>
-                <div className={s.dots}>
+                <button className={s.slideButton}>ПОДРОБНЕЕ</button>
+                <div className={s.slideDots}>
                     <div className={s.firstDot}></div>
                     <div className={s.secondDot}></div>
                     <div className={s.thirdDot}></div>
                     <div className={s.fourthDot}></div>
                     <div className={s.fivethDot}></div>
-                </div>
-                <div className={s.arrowLeft}>
-                    <div className={s.arrowLeftLineOne}></div>
-                    <div className={s.arrowLeftLineTwo}></div>
-                </div>
-                <div className={s.arrowRight}>
-                    <div className={s.arrowRightLineOne}></div>
-                    <div className={s.arrowRightLineTwo}></div>
                 </div>
             </div>
             <div className={s.slideFiveth}>
-                <div className={s.sliderHeader}>
+                <div className={s.slideHeader}>
                     СМЕСИТЕЛИ<br />
                     RAIBER
                 </div>
-                <div className={s.sliderSubheader}>
+                <div className={s.slideSubheader}>
                     уже в наличии
                 </div>
-                <button className={s.sliderButton}>ПОДРОБНЕЕ</button>
-                <div className={s.dots}>
+                <button className={s.slideButton}>ПОДРОБНЕЕ</button>
+                <div className={s.slideDots}>
                     <div className={s.firstDot}></div>
                     <div className={s.secondDot}></div>
                     <div className={s.thirdDot}></div>
                     <div className={s.fourthDot}></div>
                     <div className={s.fivethDot}></div>
                 </div>
-                <div className={s.arrowLeft}>
-                    <div className={s.arrowLeftLineOne}></div>
-                    <div className={s.arrowLeftLineTwo}></div>
-                </div>
-                <div className={s.arrowRight}>
-                    <div className={s.arrowRightLineOne}></div>
-                    <div className={s.arrowRightLineTwo}></div>
-                </div>
+            </div>
+            <div className={s.arrowLeft}>
+            </div>
+            <div className={s.arrowRight}>
             </div>
         </div>
     )
