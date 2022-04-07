298377689b9a577093507218b52aeff730253649
Max <lord180499@gmail.com>
2022-04-07 15:23:51 +0300

    create Slider component

--- src/Components/Banners/Banners.tsx
+++ src/Components/Banners/Banners.tsx
@@ -1,54 +1,31 @@
 import { FC } from "react";
 import s from "./Banners.module.sass";
+import Slider from "./Slider/Slider";
 
 const Banners: FC = () => {
-    return (
-        <section className={s.banners}>
-            <div className={s.bannersSlider}>
-                <div className={s.sliderHeader}>
-                    СМЕСИТЕЛИ<br />
-                    RAIBER
-                </div>
-                <div className={s.sliderSubheader}>
-                    уже в наличии
-                </div>
-                <button className={s.sliderButton}>ПОДРОБНЕЕ</button>
-                <div className={s.dots}>
-                    <div className={s.firstDot}></div>
-                    <div className={s.secondDot}></div>
-                    <div className={s.thirdDot}></div>
-                    <div className={s.fourthDot}></div>
-                    <div className={s.fivethDot}></div>
-                </div>
-                <div className={s.arrowLeft}>
-                    <div className={s.arrowLeftLineOne}></div>
-                    <div className={s.arrowLeftLineTwo}></div>
-                </div>
-                <div className={s.arrowRight}>
-                    <div className={s.arrowRightLineOne}></div>
-                    <div className={s.arrowRightLineTwo}></div>
-                </div>
-            </div>
-            <div className={s.bannersTop}>
-                <div className={s.bannerHeader}>
-                    УНИТАЗЫ<br />
-                    <p>ДО 10 000 ₽</p>
-                </div>
-                <div className={s.bannerSubheader}>
-                    ПОДРОБНЕЕ
-                </div>
-            </div>
-            <div className={s.bannersBot}>
-                <div className={s.bannerHeader}>
-                    ТОВАРЫ<br />
-                    <p>СО СКИДКОЙ</p>
-                </div>
-                <div className={s.bannerSubheader}>
-                    ПОДРОБНЕЕ
-                </div>
-            </div>
-        </section>
-    )
+	return (
+		<section className={s.banners}>
+			<Slider />
+			<div className={s.bannersTop}>
+				<div className={s.bannerHeader}>
+					УНИТАЗЫ<br />
+					<p>ДО 10 000 ₽</p>
+				</div>
+				<div className={s.bannerSubheader}>
+					ПОДРОБНЕЕ
+				</div>
+			</div>
+			<div className={s.bannersBot}>
+				<div className={s.bannerHeader}>
+					ТОВАРЫ<br />
+					<p>СО СКИДКОЙ</p>
+				</div>
+				<div className={s.bannerSubheader}>
+					ПОДРОБНЕЕ
+				</div>
+			</div>
+		</section>
+	)
 }
 
 export default Banners;
---
+++ src/Components/Banners/Slider/Slider.tsx
@@ -0,0 +1,136 @@
+import { FC } from "react";
+import s from "./Slider.module.sass";
+
+const Slider: FC = () => {
+    return (
+        <div className={s.slider}>
+            <div className={s.slideFirst}>
+                <div className={s.slideHeader}>
+                    СМЕСИТЕЛИ<br />
+                    RAIBER
+                </div>
+                <div className={s.slideSubheader}>
+                    уже в наличии
+                </div>
+                <button className={s.slideButton}>ПОДРОБНЕЕ</button>
+                <div className={s.dots}>
+                    <div className={s.firstDot}></div>
+                    <div className={s.secondDot}></div>
+                    <div className={s.thirdDot}></div>
+                    <div className={s.fourthDot}></div>
+                    <div className={s.fivethDot}></div>
+                </div>
+                <div className={s.arrowLeft}>
+                    <div className={s.arrowLeftLineOne}></div>
+                    <div className={s.arrowLeftLineTwo}></div>
+                </div>
+                <div className={s.arrowRight}>
+                    <div className={s.arrowRightLineOne}></div>
+                    <div className={s.arrowRightLineTwo}></div>
+                </div>
+            </div>
+            <div className={s.slideSecond}>
+                <div className={s.slideHeader}>
+                    СМЕСИТЕЛИ<br />
+                    RAIBER
+                </div>
+                <div className={s.slideSubheader}>
+                    уже в наличии
+                </div>
+                <button className={s.slideButton}>ПОДРОБНЕЕ</button>
+                <div className={s.dots}>
+                    <div className={s.firstDot}></div>
+                    <div className={s.secondDot}></div>
+                    <div className={s.thirdDot}></div>
+                    <div className={s.fourthDot}></div>
+                    <div className={s.fivethDot}></div>
+                </div>
+                <div className={s.arrowLeft}>
+                    <div className={s.arrowLeftLineOne}></div>
+                    <div className={s.arrowLeftLineTwo}></div>
+                </div>
+                <div className={s.arrowRight}>
+                    <div className={s.arrowRightLineOne}></div>
+                    <div className={s.arrowRightLineTwo}></div>
+                </div>
+            </div>
+            <div className={s.slideThird}>
+                <div className={s.slideHeader}>
+                    СМЕСИТЕЛИ<br />
+                    RAIBER
+                </div>
+                <div className={s.slideSubheader}>
+                    уже в наличии
+                </div>
+                <button className={s.slideButton}>ПОДРОБНЕЕ</button>
+                <div className={s.dots}>
+                    <div className={s.firstDot}></div>
+                    <div className={s.secondDot}></div>
+                    <div className={s.thirdDot}></div>
+                    <div className={s.fourthDot}></div>
+                    <div className={s.fivethDot}></div>
+                </div>
+                <div className={s.arrowLeft}>
+                    <div className={s.arrowLeftLineOne}></div>
+                    <div className={s.arrowLeftLineTwo}></div>
+                </div>
+                <div className={s.arrowRight}>
+                    <div className={s.arrowRightLineOne}></div>
+                    <div className={s.arrowRightLineTwo}></div>
+                </div>
+            </div>
+            <div className={s.slideFourth}>
+                <div className={s.sliderHeader}>
+                    СМЕСИТЕЛИ<br />
+                    RAIBER
+                </div>
+                <div className={s.sliderSubheader}>
+                    уже в наличии
+                </div>
+                <button className={s.sliderButton}>ПОДРОБНЕЕ</button>
+                <div className={s.dots}>
+                    <div className={s.firstDot}></div>
+                    <div className={s.secondDot}></div>
+                    <div className={s.thirdDot}></div>
+                    <div className={s.fourthDot}></div>
+                    <div className={s.fivethDot}></div>
+                </div>
+                <div className={s.arrowLeft}>
+                    <div className={s.arrowLeftLineOne}></div>
+                    <div className={s.arrowLeftLineTwo}></div>
+                </div>
+                <div className={s.arrowRight}>
+                    <div className={s.arrowRightLineOne}></div>
+                    <div className={s.arrowRightLineTwo}></div>
+                </div>
+            </div>
+            <div className={s.slideFiveth}>
+                <div className={s.sliderHeader}>
+                    СМЕСИТЕЛИ<br />
+                    RAIBER
+                </div>
+                <div className={s.sliderSubheader}>
+                    уже в наличии
+                </div>
+                <button className={s.sliderButton}>ПОДРОБНЕЕ</button>
+                <div className={s.dots}>
+                    <div className={s.firstDot}></div>
+                    <div className={s.secondDot}></div>
+                    <div className={s.thirdDot}></div>
+                    <div className={s.fourthDot}></div>
+                    <div className={s.fivethDot}></div>
+                </div>
+                <div className={s.arrowLeft}>
+                    <div className={s.arrowLeftLineOne}></div>
+                    <div className={s.arrowLeftLineTwo}></div>
+                </div>
+                <div className={s.arrowRight}>
+                    <div className={s.arrowRightLineOne}></div>
+                    <div className={s.arrowRightLineTwo}></div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Slider;
