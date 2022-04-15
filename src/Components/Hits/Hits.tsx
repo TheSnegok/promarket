af8c2eec819c9c03f569553546a47ca247bbff51
Max <lord180499@gmail.com>
2022-04-15 11:17:57 +0300

    work slider and animation

--- src/Components/Hits/Hits.tsx
+++ src/Components/Hits/Hits.tsx
@@ -11,9 +11,28 @@ import Item from "./Item/Item";
 const Hits: FC = () => {
 
     const [position, setPosition] = useState<number>(0);
-    const [slide, setSlide] = useState<number>(0);
-
+    const [slide, setSlide] = useState<number>(1);
     const [menuName, setMenuName] = useState<number>(0);
+
+    const changeSlide = (option: string) => {
+        if(item.length > 5)
+        switch (option) {
+            case "plus":
+                if(slide + 4 < item.length) {
+                    setPosition(position => position - 308);
+                    setSlide(slide => slide + 1);
+                }
+                break;
+            case "minus":
+                if(slide > 1) {
+                    setPosition(position => position + 308);
+                    setSlide(slide => slide - 1);
+                }
+                break;
+            default:
+                break;
+        }
+    }
     const menu: string[] = ["Любые товары", "Раковины", "Ванны", "Унитазы", "Душевые системы", "Смесители", "Зеркала", "Душевые кабины", "Стиральные машины"];
 
     const item: (string | string[])[][] = [
@@ -64,30 +83,39 @@ const Hits: FC = () => {
         ],
         [
             ["Хит"],
-            heatedTowelRail,
+            sink,
             ["full", "full", "full", "full", "half"],
-            [chat, "10"],
-            "Полотенцесушитель электрический Laris Кватро П7 40 x 60 см, 85 Вт, со...",
-            "Россия, Украина",
-            ["12 730 ₽"]
+            [chat, "12"],
+            "Раковина Roca Debba 32799400Y, 60x48 см",
+            "Испания",
+            ["2 601 ₽"]
+        ],
+        [
+            ["Хит", "Акция"],
+            bathroom,
+            ["full", "full", "full", "full", "half"],
+            [chat, "2"],
+            "Акриловая ванна AM.PM Bliss L 180х80 W53A-180-080W-ARB",
+            "Германия",
+            ["26 990 ₽", "28 601 ₽"]
         ],
         [
             ["Хит"],
-            heatedTowelRail,
-            ["full", "full", "full", "half", "Off"],
-            [chat, "10"],
-            "Полотенцесушитель электрический Laris Кватро П7 40 x 60 см, 85 Вт, со...",
-            "Россия, Украина",
-            ["12 730 ₽"]
+            shower,
+            ["full", "full", "full", "full", "half"],
+            [chat, "24"],
+            "Душевая система Raiber R0808, хром",
+            "Германия",
+            ["12 207 ₽"]
         ],
         [
             ["Хит"],
-            heatedTowelRail,
-            ["full", "full", "full", "half", "Off"],
-            [chat, "10"],
-            "Полотенцесушитель электрический Laris Кватро П7 40 x 60 см, 85 Вт, со...",
-            "Россия, Украина",
-            ["12 730 ₽"]
+            toilet,
+            ["full", "full", "full", "full", "half"],
+            [chat, "4"],
+            "Унитаз AM.PM Spirit V2.0 C708607SC компакт с сиденьем микролифт",
+            "Испания",
+            ["14 764 ₽"]
         ],
     ];
 
@@ -105,8 +133,8 @@ const Hits: FC = () => {
                         {item.map((item, index) => <Item index={index} tags={item[0]} itemImg={item[1]} itemStars={Array.isArray(item[2]) ? item[2] : undefined} message={item[3]} itemDesc={item[4]} itemCountry={item[5]} price={item[6]} />)}
                     </div>
                 </div>
-                <div className={s.arrowRight} onClick={() => setPosition(position => position - 309)}></div>
-                <div className={s.arrowLeft} onClick={() => setPosition(position => position + 309)}></div>
+                <div className={s.arrowRight} onClick={() => changeSlide("plus")}></div>
+                <div className={s.arrowLeft} onClick={() => changeSlide("minus")}></div>
             </div>
         </section>
     )
