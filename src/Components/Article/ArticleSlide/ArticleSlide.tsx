0d290f82552e27f83fddfcc162816afce275a10d
Max <lord180499@gmail.com>
2022-04-22 14:04:48 +0300

    add link for article, change svg in item

--- src/Components/Article/ArticleSlide/ArticleSlide.tsx
+++ src/Components/Article/ArticleSlide/ArticleSlide.tsx
@@ -9,10 +9,12 @@ interface IArticleSlideProps {
 const ArticleSlide = ({ index, image, articleHeader }: IArticleSlideProps) => {
     return (
         <article className={s.itemArticle} key={index}>
-            <img src={image} alt="header-image(" />
-            <div className={s.itemArticleHeader}>
-                <span>{articleHeader}</span>
-            </div>
+            <a href="/">
+                <img src={image} alt={articleHeader} />
+                <div className={s.itemArticleHeader}>
+                    <span>{articleHeader}</span>
+                </div>
+            </a>
         </article>
     )
 }
