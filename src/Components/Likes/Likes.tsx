aef1875b36286ce6db4fcfa353c6f4a05a2cca18
TheSnegok <lord180499@gmail.com>
2022-05-22 12:02:28 +0300

    changed font size and added price

--- src/Components/Likes/Likes.tsx
+++ src/Components/Likes/Likes.tsx
@@ -3,6 +3,7 @@ import s from './Likes.module.sass';
 import { useGlobalContext } from "../Context/Context";
 import { LikesItem } from "../Templates/LikesItem/LikesItem";
 import { ItemStars } from "../Templates/ItemStars/ItemStars";
+import { numberWithSpaces } from "../Templates/NumberWithSpaces/NumberWithSpaces";
 
 const Likes: FC = () => {
 
@@ -30,14 +31,27 @@ const Likes: FC = () => {
 									<div className={s.likesPreviewNull}>
 									</div>
 								) : (
-									<div className={s.likesPreviewWrapper}>
+									<div className={s.likesPreviewAnimation}>
 										{indexItem !== null && (
 											<div className={s.preview}>
 												<img src={likes.items[indexItem].imgUrl} className={s.previewImg} alt='preview-like-item' />
-												<div className={s.previewStars}>
-													<ItemStars stars={likes.items[indexItem].stars} />
+												<div className={s.previewWrapper}>
+													<div className={s.previewWrapperStars}>
+														<ItemStars stars={likes.items[indexItem].stars} />
+													</div>
+													<div className={s.previewWrapperCountry}>
+														{likes.items[indexItem].country}
+													</div>
 												</div>
 												<div className={s.previewDescription}>{likes.items[indexItem].description}</div>
+												{likes.items[indexItem].price.length === 1 ? (
+													<div className={s.previewPrice}>{numberWithSpaces(likes.items[indexItem].price[0], true)}</div>
+												) : (
+													<div className={s.previewWrapper}>
+														<div className={s.previewWrapperPrice}>{numberWithSpaces(likes.items[indexItem].price[0], true)}</div>
+														<div className={s.previewWrapperStock}>{numberWithSpaces(likes.items[indexItem].price[1], true)}</div>
+													</div>
+												)}
 											</div>
 										)}
 									</div>
