508783111f8582b007eb3a8810fe792e9f4875a7
TheSnegok <lord180499@gmail.com>
2022-05-24 13:41:44 +0300

    added some style and numberWithSpaces

--- src/Components/Templates/BasketItem/BasketItem.tsx
+++ src/Components/Templates/BasketItem/BasketItem.tsx
@@ -1,4 +1,5 @@
 import { ILikesItems } from "../../Context/Context";
+import { numberWithSpaces } from "../NumberWithSpaces/NumberWithSpaces";
 import s from "./BasketItem.module.sass";
 
 interface IBasketItem {
@@ -11,7 +12,7 @@ export const BasketItem = ({ item }: IBasketItem) => {
             <img src={item.imgUrl} alt={item.description} className={s.itemImg} />
             <span className={s.itemDescription}>{item.description}</span>
             <span className={s.itemCountry}>{item.country}</span>
-            <span className={s.itemPrice}>{item.price}</span>
+            <span className={s.itemPrice}>{numberWithSpaces(item.price[0])}</span>
         </li>
     )
 } 
