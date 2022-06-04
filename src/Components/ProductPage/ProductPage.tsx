dcdc0f3a72ef6c295a58061c19d4436058665c72
TheSnegok <lord180499@gmail.com>
2022-06-04 19:26:43 +0300

    styled a bit productPage

--- src/Components/ProductPage/ProductPage.tsx
+++ src/Components/ProductPage/ProductPage.tsx
@@ -3,6 +3,7 @@ import { useGlobalContext } from "../Context/Context"
 import { ItemStars } from "../Templates/ItemStars/ItemStars";
 import s from "./ProductPage.module.sass"
 import chat from "../../Image/hits/messageLogo.svg"
+import { numberWithSpaces } from "../Templates/NumberWithSpaces/NumberWithSpaces";
 
 export const ProductPage = () => {
 
@@ -31,32 +32,31 @@ export const ProductPage = () => {
 								<div className={s.productInfoMainFullname}>
 									<h3>{product.description}</h3>
 								</div>
-								<div className={s.productInfoReviews}>
-									<div className={s.productInfoReviewsStars}>
+								<div className={s.productInfoMainReviews}>
+									<div className={s.productInfoMainReviewsStars}>
 										<ItemStars stars={product.stars} />
 									</div>
-									<div className={s.productInfoReviewsCount}>
+									<div className={s.productInfoMainReviewsCount}>
 										<img src={chat} alt="chat" />
 									</div>
 								</div>
 								{product.price[1] ? (
 									<div className={s.productInfoMainPrice}>
-										<div className={s.productInfoMainPricePrevious}><span>{product.price[1]}</span></div>
-										<div className={s.productInfoMainPriceStock}><span>{product.price[0]}</span></div>
+										<div className={s.productInfoMainPricePrevious}><span>{numberWithSpaces(product.price[1], true)}</span></div>
+										<div className={s.productInfoMainPriceStock}><span>{numberWithSpaces(product.price[0], true)}</span></div>
 									</div>
 								) : (
 									<div className={s.productInfoMainPrice}>
-										<div className={s.productInfoMainPriceDefault}><span>{product.price[0]}</span></div>
+										<div className={s.productInfoMainPriceDefault}><span>{numberWithSpaces(product.price[0], true)}</span></div>
 									</div>
 								)}
 							</div>
 						</div>
 						<div className={s.productCharacteristic}>
 							<div className={s.productCharacteristicVideo}>
-								<iframe width="60vw" height="20vh" src="https://www.youtube.com/embed/KOqs4WUhgVk" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
+								<iframe width="90%" height="90%" src="https://www.youtube.com/embed/KOqs4WUhgVk" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
 							</div>
 							<div className={s.productCharacteristicReviews}>
-								/* product.reviews.slice(0, 5).map() */
 								<div className={s.productCharacteristicReviewsItem}>
 									<span>Vasya Pupkin</span>
 									<span>
@@ -96,4 +96,4 @@ export const ProductPage = () => {
 		</>
 	)
 }
-// same type, +-price in product similars
+// same type, +-price in product similars, product.reviews.slice(0, 5).map()
