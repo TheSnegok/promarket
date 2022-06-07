6092a1b31909e93a2a0542bb0dd9c37409e65605
TheSnegok <lord180499@gmail.com>
2022-06-07 18:45:06 +0300

    added button and some styled

--- src/Components/ProductPage/ProductPage.tsx
+++ src/Components/ProductPage/ProductPage.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Navigate } from "react-router";
 import { useGlobalContext } from "../Context/Context"
 import { ItemStars } from "../Templates/ItemStars/ItemStars";
@@ -8,98 +9,123 @@ import { AddsClassForTags } from "../Templates/AddedClassForTag/AddsClassForTags
 
 export const ProductPage = () => {
 
-	const { product } = useGlobalContext();
+	const { product, contextFindItem, contextRemoveItem, contextPushItem } = useGlobalContext();
 
+	const [like, setLike] = useState<boolean>(contextFindItem('likes', product!.personalKey) ? false : true);
+	const [buy, setBuy] = useState<boolean>(contextFindItem('basket', product!.personalKey) ? false : true);
+
+	const clickSetIn = (itemName: string) => {
+		if (itemName === 'likes') {
+			if (like) {
+				setLike(false);
+				contextRemoveItem('likes', product!.personalKey);
+			} else {
+				setLike(true);
+				contextPushItem('likes', product!);
+			}
+		} else if (itemName === 'basket') {
+			if (buy) {
+				setBuy(false);
+				contextRemoveItem('basket', product!.personalKey);
+			} else {
+				setBuy(true);
+				contextPushItem('basket', product!);
+			}
+		}
+	};
+
+	if (product === null) {
+		return <Navigate to="/" replace={true} />
+	}
 	return (
-		<>
-			{product === null ? <Navigate to="/" replace={true} />
-				: (
-					<section className={s.product}>
-						<div className={s.productHeader}>
-							<div className={s.productHeaderText}>
-								<h1>{product.description}</h1>
-							</div>
-							<div className={s.productHeaderTypes}>
-								{product.type.map(element => AddsClassForTags(element))}
-							</div>
+		<section className={s.product}>
+			<div className={s.productHeader}>
+				<div className={s.productHeaderText}>
+					<h1>{product.description}</h1>
+				</div>
+				<div className={s.productHeaderTypes}>
+					{product.type.map(element => AddsClassForTags(element))}
+				</div>
+			</div>
+			<div className={s.productInfo}>
+				<div className={s.productInfoImg}>
+					<img src={product.imgUrl} alt={product.description} />
+					<div className={s.productInfoImgOther}>
+						<img src={product.imgUrl} alt={product.description} />
+						<img src={product.imgUrl} alt={product.description} />
+						<img src={product.imgUrl} alt={product.description} />
+						<img src={product.imgUrl} alt={product.description} />
+						<img src={product.imgUrl} alt={product.description} />
+					</div>
+				</div>
+				<div className={s.productInfoMain}>
+					<div className={s.productInfoMainFullname}>
+						<h3>{product.description}</h3>
+					</div>
+					<div className={s.productInfoMainReviews}>
+						<div className={s.productInfoMainReviewsStars}>
+							<ItemStars stars={product.stars} />
 						</div>
-						<div className={s.productInfo}>
-							<div className={s.productInfoImg}>
-								<img src={product.imgUrl} alt={product.description} />
-								<div className={s.productInfoImgOther}>
-									<img src={product.imgUrl} alt={product.description} />
-									<img src={product.imgUrl} alt={product.description} />
-									<img src={product.imgUrl} alt={product.description} />
-									<img src={product.imgUrl} alt={product.description} />
-									<img src={product.imgUrl} alt={product.description} />
-								</div>
-							</div>
-							<div className={s.productInfoMain}>
-								<div className={s.productInfoMainFullname}>
-									<h3>{product.description}</h3>
-								</div>
-								<div className={s.productInfoMainReviews}>
-									<div className={s.productInfoMainReviewsStars}>
-										<ItemStars stars={product.stars} />
-									</div>
-									<div className={s.productInfoMainReviewsCount}>
-										<img src={chat} alt="chat" />
-									</div>
-								</div>
-								{product.price[1] ? (
-									<div className={s.productInfoMainPrice}>
-										<div className={s.productInfoMainPricePrevious}><span>{numberWithSpaces(product.price[1], true)}</span></div>
-										<div className={s.productInfoMainPriceStock}><span>{numberWithSpaces(product.price[0], true)}</span></div>
-									</div>
-								) : (
-									<div className={s.productInfoMainPrice}>
-										<div className={s.productInfoMainPriceDefault}><span>{numberWithSpaces(product.price[0], true)}</span></div>
-									</div>
-								)}
-							</div>
+						<div className={s.productInfoMainReviewsCount}>
+							<img src={chat} alt="chat" />
 						</div>
-						<div className={s.productCharacteristic}>
-							<div className={s.productCharacteristicVideo}>
-								<iframe width="90%" height="90%" src="https://www.youtube.com/embed/KOqs4WUhgVk" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
-							</div>
-							<div className={s.productCharacteristicReviews}>
-								<div className={s.productCharacteristicReviewsItem}>
-									<span>Vasya Pupkin</span>
-									<span>
-										Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, magni ea debitis sapiente blanditiis maiores non ipsum itaque illum fugit, quo assumenda quia? Molestias incidunt, assumenda corporis ratione itaque voluptatem?
-									</span>
-								</div>
-								<div className={s.productCharacteristicReviewsItem}>
-									<span>Vasya Pupkin</span>
-									<span>
-										Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, magni ea debitis sapiente blanditiis maiores non ipsum itaque illum fugit, quo assumenda quia? Molestias incidunt, assumenda corporis ratione itaque voluptatem?
-									</span>
-								</div>
-								<div className={s.productCharacteristicReviewsItem}>
-									<span>Vasya Pupkin</span>
-									<span>
-										Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, magni ea debitis sapiente blanditiis maiores non ipsum itaque illum fugit, quo assumenda quia? Molestias incidunt, assumenda corporis ratione itaque voluptatem?
-									</span>
-								</div>
-								<div className={s.productCharacteristicReviewsItem}>
-									<span>Vasya Pupkin</span>
-									<span>
-										Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, magni ea debitis sapiente blanditiis maiores non ipsum itaque illum fugit, quo assumenda quia? Molestias incidunt, assumenda corporis ratione itaque voluptatem?
-									</span>
-								</div>
-								<div className={s.productCharacteristicReviewsItem}>
-									<span>Vasya Pupkin</span>
-									<span>
-										Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, magni ea debitis sapiente blanditiis maiores non ipsum itaque illum fugit, quo assumenda quia? Molestias incidunt, assumenda corporis ratione itaque voluptatem?
-									</span>
-								</div>
-							</div>
+					</div>
+					{product.price[1] ? (
+						<div className={s.productInfoMainPrice}>
+							<div className={s.productInfoMainPricePrevious}><span>{numberWithSpaces(product.price[1], true)}</span></div>
+							<div className={s.productInfoMainPriceStock}><span>{numberWithSpaces(product.price[0], true)}</span></div>
 						</div>
-						<div className={s.productSimilars}>
+					) : (
+						<div className={s.productInfoMainPrice}>
+							<div className={s.productInfoMainPriceDefault}><span>{numberWithSpaces(product.price[0], true)}</span></div>
 						</div>
-					</section>
-				)}
-		</>
+					)}
+					<div className={s.productInfoMainAdd}>
+						<button className={s.productInfoMainAddBuy} onClick={() => clickSetIn('basket')}>{buy ? 'В корзине' : 'В корзину'}</button>
+						<button className={s.productInfoMainAddLike} onClick={() => clickSetIn('likes')}>{like ? 'Нравится' : 'Не нравится'}</button>
+					</div>
+				</div>
+			</div>
+			<div className={s.productCharacteristic}>
+				<div className={s.productCharacteristicVideo}>
+					<iframe width="90%" height="90%" src="https://www.youtube.com/embed/KOqs4WUhgVk" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
+				</div>
+				<div className={s.productCharacteristicReviews}>
+					<div className={s.productCharacteristicReviewsItem}>
+						<span>Vasya Pupkin</span>
+						<span>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, magni ea debitis sapiente blanditiis maiores non ipsum itaque illum fugit, quo assumenda quia? Molestias incidunt, assumenda corporis ratione itaque voluptatem?
+						</span>
+					</div>
+					<div className={s.productCharacteristicReviewsItem}>
+						<span>Vasya Pupkin</span>
+						<span>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, magni ea debitis sapiente blanditiis maiores non ipsum itaque illum fugit, quo assumenda quia? Molestias incidunt, assumenda corporis ratione itaque voluptatem?
+						</span>
+					</div>
+					<div className={s.productCharacteristicReviewsItem}>
+						<span>Vasya Pupkin</span>
+						<span>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, magni ea debitis sapiente blanditiis maiores non ipsum itaque illum fugit, quo assumenda quia? Molestias incidunt, assumenda corporis ratione itaque voluptatem?
+						</span>
+					</div>
+					<div className={s.productCharacteristicReviewsItem}>
+						<span>Vasya Pupkin</span>
+						<span>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, magni ea debitis sapiente blanditiis maiores non ipsum itaque illum fugit, quo assumenda quia? Molestias incidunt, assumenda corporis ratione itaque voluptatem?
+						</span>
+					</div>
+					<div className={s.productCharacteristicReviewsItem}>
+						<span>Vasya Pupkin</span>
+						<span>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, magni ea debitis sapiente blanditiis maiores non ipsum itaque illum fugit, quo assumenda quia? Molestias incidunt, assumenda corporis ratione itaque voluptatem?
+						</span>
+					</div>
+				</div>
+			</div>
+			<div className={s.productSimilars}>
+			</div>
+		</section>
 	)
 }
 // same type, +-price in product similars, product.reviews.slice(0, 5).map()
