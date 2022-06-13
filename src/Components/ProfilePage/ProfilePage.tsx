46aa72d244127bf0ace906cf9b11d1009901af69
TheSnegok <lord180499@gmail.com>
2022-06-13 13:42:53 +0300

    styled profile page

--- src/Components/ProfilePage/ProfilePage.tsx
+++ src/Components/ProfilePage/ProfilePage.tsx
@@ -1,26 +1,31 @@
 import s from "./ProfilePage.module.sass";
+import cabinet from '../../Image/Group.svg';
 
 export const ProfilePage = () => {
     return (
         <section className={s.profilePage}>
             <div className={s.profilePageHeader}>
-                <h3>Профиль</h3>
+                <h1>Профиль</h1>
             </div>
             <div className={s.profilePageMain}>
                 <div className={s.profilePageMainAva}>
-                    <img src="" alt="ava" />
+                    <img src={cabinet} alt="ava" />
                 </div>
                 <div className={s.profilePageMainInfo}>
                     <div className={s.profilePageMainInfoName}>
+                        <label>Ваше имя:</label>
                         <input type="text" value="Max" />
                     </div>
                     <div className={s.profilePageMainInfoSecondname}>
+                        <label>Ваша фамилия:</label>
                         <input type="text" value="Pfjsdof"/>
                     </div>
                     <div className={s.profilePageMainInfoNumber}>
+                        <label>Ваш телефон:</label>
                         <input type="tel" name="" />
                     </div>
                     <div className={s.profilePageMainInfoPassword}>
+                        <label>Ваш пароль:</label>
                         <input type="password" />
                     </div>
 
