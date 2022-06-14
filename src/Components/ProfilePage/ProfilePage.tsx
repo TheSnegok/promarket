c1a59911e032859039eaeab91682a57be83d04b6
TheSnegok <lord180499@gmail.com>
2022-06-14 17:41:42 +0300

    added input for avatar

--- src/Components/ProfilePage/ProfilePage.tsx
+++ src/Components/ProfilePage/ProfilePage.tsx
@@ -1,5 +1,6 @@
 import s from "./ProfilePage.module.sass";
 import cabinet from '../../Image/Group.svg';
+import camera from '../../Image/CameraPhoto.svg';
 
 export const ProfilePage = () => {
     return (
@@ -8,30 +9,36 @@ export const ProfilePage = () => {
                 <h1>Профиль</h1>
             </div>
             <div className={s.profilePageMain}>
-                <div className={s.profilePageMainAva}>
-                    <img src={cabinet} alt="ava" />
-                    <input type="file" name="ava" />
+                <div className={s.profilePageMainBlock}>
+                    <div className={s.profilePageMainBlockAva}>
+                        <img src={cabinet} alt="ava" className={s.profilePageMainBlockAvaImg} />
+                        <label htmlFor="ava">
+                            <img src={camera} alt="camera" />
+                            <span>Добавить фото</span>
+                        </label>
+                        <input type="file" id="ava" />
+                    </div>
                 </div>
                 <div className={s.profilePageMainInfo}>
                     <div className={s.profilePageMainInfoName}>
                         <label htmlFor="name" >Ваше имя:</label>
-                        <input name="name" type="text" defaultValue="Max" />
+                        <input id="name" type="text" defaultValue="Max" />
                     </div>
                     <div className={s.profilePageMainInfoSurname}>
                         <label htmlFor="surname">Ваша фамилия:</label>
-                        <input name="Surname" type="text" defaultValue="Pfjsdof"/>
+                        <input id="surname" type="text" defaultValue="Pfjsdof" />
                     </div>
                     <div className={s.profilePageMainInfoNumber}>
                         <label htmlFor="tel">Ваш телефон:</label>
-                        <input type="tel" name="tel" />
+                        <input type="tel" id="tel" />
                     </div>
                     <div className={s.profilePageMainInfoEmail}>
                         <label htmlFor="email">Ваш емеил:</label>
-                        <input type="email" name="email" />
+                        <input type="email" id="email" />
                     </div>
                     <div className={s.profilePageMainInfoPassword}>
                         <label htmlFor="password">Ваш пароль:</label>
-                        <input type="password" name="password"/>
+                        <input type="password" id="password" />
                     </div>
                 </div>
             </div>
