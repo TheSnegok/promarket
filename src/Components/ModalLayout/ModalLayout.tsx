7f9cb33f842e92e1857a5dfa22f181a5bc9b73f2
TheSnegok <lord180499@gmail.com>
2022-06-22 22:34:20 +0300

    added tags

--- src/Components/ModalLayout/ModalLayout.tsx
+++ src/Components/ModalLayout/ModalLayout.tsx
@@ -2,36 +2,46 @@ import { useEffect } from "react";
 import { useGlobalContext } from "../Context/Context";
 import { ItemStars } from "../Templates/ItemStars/ItemStars";
 import s from "./ModalLayout.module.sass";
+import chat from "../../Image/hits/messageLogo.svg";
+import { AddsClassForTags } from "../Templates/AddedClassForTag/AddsClassForTags";
 
 export const ModalLayout = () => {
 
-    const { modal, setModal } = useGlobalContext();
+	const { modal, setModal } = useGlobalContext();
 
-    useEffect(() => {
-        modal.visible ? document.body.style.overflow = "hidden" : document.body.style.overflow = "unset";
-    }, [modal]);
+	useEffect(() => {
+		modal.visible ? document.body.style.overflow = "hidden" : document.body.style.overflow = "unset";
+	}, [modal]);
 
-    return (
-        <section className={modal.visible ? s.modal : s.modalHidden}>
-            <div className={s.modalBlock}>
-                <div className={s.modalBlockHeader}>
-                    <div className={s.modalBlockHeaderName}>{modal.item?.description}</div>
-                    <div className={s.modalBlockHeaderClose} onClick={() => setModal({
-                        visible: false,
-                        item: null
-                    })}>Close</div>
-                </div>
-                <div className={s.modalBlockInfo}>
-                    <div className={s.modalBlockHeaderImage}>
-                        <img src={modal.item?.imgUrl} alt="product-preview-ploto" />
-                    </div>
-                    <div className={s.modalBlockInfoOptions}>
-                        <div className={s.modalBlockInfoOptionsStars}>
-                            <ItemStars stars={modal.item!.stars}  />
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-    )
+	return (
+		<section className={modal.visible ? s.modal : s.modalHidden}>
+			<div className={s.modalBlock}>
+				<div className={s.modalBlockHeader}>
+					<div className={s.modalBlockHeaderName}>
+						{modal.item?.description}
+						{modal.item?.type.map(tag => AddsClassForTags(tag))}
+					</div>
+					<div className={s.modalBlockHeaderClose} onClick={() => setModal({
+						visible: false,
+						item: null
+					})}>Close</div>
+				</div>
+				<div className={s.modalBlockInfo}>
+					<div className={s.modalBlockHeaderImage}>
+						<img src={modal.item?.imgUrl} alt="product-preview-ploto" />
+					</div>
+					<div className={s.modalBlockInfoOptions}>
+						<div className={s.modalBlockInfoOptionsReviews}>
+							{modal.item && <ItemStars stars={modal.item.stars} />}
+							<div className={s.modalBlockInfoOptionsReviewsMessage}>
+								<img src={chat} alt="chat" />
+								<span className={s.messageCount}>{modal.item?.review}</span>
+							</div>
+						</div>
+
+					</div>
+				</div>
+			</div>
+		</section>
+	)
 }
