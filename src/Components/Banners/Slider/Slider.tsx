f14feac597482329d07c9f5cb5d603c435eb4fdb
TheSnegok <lord180499@gmail.com>
2022-07-07 18:55:32 +0300

    refactored slider for easy makes responsive

--- src/Components/Banners/Slider/Slider.tsx
+++ src/Components/Banners/Slider/Slider.tsx
@@ -4,33 +4,27 @@ import s from "./Slider.module.sass";
 
 const Slider: FC = () => {
 
-    const [position, setPosition] = useState<number>(0);
-    const [dot, setDot] = useState<number>(0);
+    const [slide, setSlide] = useState<number>(0);
 
-    const changeSlide = (option: string, item?: number, index?: number) => {
+    const changeSlide = (option: string, index?: number) => {
         switch (option) {
             case "dot":
-                if (item !== undefined && index !== undefined) {
-                    setPosition(item);
-                    setDot(index);
+                if (index !== undefined) {
+                    setSlide(index);
                 }
                 break;
             case "minus":
-                if (position === 0) {
-                    setPosition(-400);
-                    setDot(4);
+                if (slide === 0) {
+                    setSlide(4);
                 } else {
-                    setPosition(position => position + 100 );
-                    setDot(dot => dot - 1);
+                    setSlide(slide => slide - 1);
                 }
                 break;
             case "plus":
-                if (position === -400) {
-                    setPosition(0);
-                    setDot(0);
+                if (slide === 4) {
+                    setSlide(0);
                 } else {
-                    setPosition(position => position - 100);
-                    setDot(dot => dot + 1);
+                    setSlide(slide => slide + 1);
                 }
                 break;
             default:
@@ -48,11 +42,11 @@ const Slider: FC = () => {
 
     return (
         <div className={s.slider}>
-            <div className={s.slides} style={{ left: position + '%' }}>
+            <div className={slide === 0 ? s.first : slide === 1 ? s.second : slide === 2 ? s.third : slide === 3 ? s.fourth : slide === 4 ? s.fiveth : s.first} >
                 {slides.map((item: string[], index: number) => <Slide key={index} headerText={item[0]} subHeaderText={item[1]} buttonText={item[2]} />)}
             </div>
-            <div className={s.slidesDots}>
-                {slides.map((item, index: number) => <div key={index} className={dot === index ? s.DotActive : s.Dot} onClick={() => changeSlide("dot", (index * -100), index)} />)}
+            <div className={s.sliderDots}>
+                {slides.map((item, index: number) => <div key={index} className={slide === index ? s.DotActive : s.Dot} onClick={() => changeSlide("dot", index)} />)}
             </div>
             <div className={s.arrowLeft} onClick={() => changeSlide("minus")}>
             </div>
