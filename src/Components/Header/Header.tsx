0930df331a3db28395be9b8e264f87a8d6cce71a
TheSnegok <lord180499@gmail.com>
2022-08-09 19:56:51 +0300

    refactored header drops

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react'
+import { useRef, FC, useEffect, useState } from 'react'
 import s from "./Header.module.sass"
 import logo from '../../Image/logo.svg';
 import mark from '../../Image/Subtract.svg';
@@ -7,7 +7,12 @@ import { useGlobalContext } from '../Context/Context';
 
 const Header: FC = () => {
 
+	const regSpan = useRef<HTMLSpanElement | null>(null);
+
 	const { data, callInput, setCallInput, region, setRegion } = useGlobalContext();
+
+	const [call, setCall] = useState<boolean>(false);
+	const [regList, setRegList] = useState<boolean>(false);
 
 	return (
 		<header className={s.header}>
@@ -19,8 +24,8 @@ const Header: FC = () => {
 			<div className={s.headerLocation}>
 				<img src={mark} alt="mark" className={s.mark} />
 				<div className={s.regions}>
-					<span>{region}</span>
-					<div className={s.regionsList}>
+					<span onClick={() => setRegList(regList => !regList)} ref={regSpan}>{region}</span>
+					<div className={regList ? s.regionsListVisible : s.regionsList}>
 						<ul>
 							{data.regionsList.map((item, index) => {
 								return (
@@ -38,13 +43,13 @@ const Header: FC = () => {
 					<a href="tel:+84950183210">8 495 018-32-10</a>
 				</div>
 				<div className={s.call}>
-					<div className={s.popup}>
+					<div className={call ? s.popupDown : s.popup}>
 					</div>
 					<div className={s.phoneCall}>
-						<span>Заказать звонок</span>
-						<form action='/' className={s.phoneCallInput}>
-							<input type="text" pattern={'^([\u0401\u0451\u0410-\u044f]{1,20})'} title={'Ваше имя должно содержать от 2 букв до 24 букв!'} placeholder={'Введите ваше имя...'} value={callInput.name} onChange={e => setCallInput({name: e.target.value, phone: callInput.phone})} />
-							<input type="tel" pattern={'^([+]?[\s0-9]+)?(\d{3}|[(]?[0-9]+[)])?([-]?[\s]?[0-9])+$'} title={`Введите настоящий номер телефона содержащий 10 цифр!`} placeholder={'Введите номер телофона...'} value={callInput.phone} onChange={e => setCallInput({name: callInput.name, phone: e.target.value})} />
+						<span onClick={() => setCall(call => !call)}>Заказать звонок</span>
+						<form action='/' className={call ? s.visiblePhoneCallInput : s.phoneCallInput}>
+							<input type="text" pattern={'^([\u0401\u0451\u0410-\u044f]{1,20})'} title={'Ваше имя должно содержать от 2 букв до 24 букв!'} placeholder={'Введите ваше имя...'} value={callInput.name} onChange={e => setCallInput({ name: e.target.value, phone: callInput.phone })} />
+							<input type="tel" pattern={'^([+]?[\s0-9]+)?(\d{3}|[(]?[0-9]+[)])?([-]?[\s]?[0-9])+$'} title={`Введите настоящий номер телефона содержащий 10 цифр!`} placeholder={'Введите номер телофона...'} value={callInput.phone} onChange={e => setCallInput({ name: callInput.name, phone: e.target.value })} />
 							<button>Заказать звонок</button>
 						</form>
 					</div>
