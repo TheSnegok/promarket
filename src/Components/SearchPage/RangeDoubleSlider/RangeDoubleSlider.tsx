d9e4913f1db20a76bd02339c87c3c648e7e54ea1
TheSnegok <lord180499@gmail.com>
2022-09-12 20:56:24 +0300

    maked worked RangeDoubleSlider without sort items

--- src/Components/SearchPage/RangeDoubleSlider/RangeDoubleSlider.tsx
+++ src/Components/SearchPage/RangeDoubleSlider/RangeDoubleSlider.tsx
@@ -1,26 +1,45 @@
-import { FC } from "react";
+import { FC, FormEvent, MutableRefObject, useRef, useState } from "react";
 import s from "./RangeDoubleSlider.module.sass";
 
 interface RangeDoubleSliderProps {
     header: string;
 }
 
-export const RangeDoubleSlider:FC<RangeDoubleSliderProps> = ({ header }) => {
+export const RangeDoubleSlider: FC<RangeDoubleSliderProps> = ({ header }) => {
+
+    const [leftSlider, setLeftSlider] = useState<number>(0);
+    const [rightSlider, setRightSlider] = useState<number>(50000);
+    const leftDot = useRef<HTMLInputElement>(null);
+    const rightDot = useRef<HTMLInputElement>(null);
+    const maxValue = useRef<HTMLInputElement>(null);
+    const minValue = useRef<HTMLInputElement>(null);
+
+    const changeValue = (e: FormEvent, side: string) => {
+        e.preventDefault();
+        if (rightDot.current !== null && leftDot.current !== null) {
+            if (side === 'right' && +rightDot.current.value > leftSlider+1000 ) {
+                setRightSlider(+rightDot.current.value);
+            } else if (side === 'left' && +leftDot.current.value < rightSlider-1000) {
+                setLeftSlider(+leftDot.current.value);
+            }
+        }
+    }
+
     return (
         <div className={s.rangeBlock}>
             <span className={s.rangeBlockText}>{header}</span>
             <div className={s.rangeBlockInput}>
-                <input type="range" name="" id="" className={s.rangeBlockInputLeft} />
-                <input type="range" name="" id="" className={s.rangeBlockInputRight} />
+                <input min='0' max='50000' step={1} value={leftSlider} ref={leftDot} type="range" onInput={(e) => changeValue(e, 'left')} className={s.rangeBlockInputLeft} />
+                <input min='0' max='50000' step={1} value={rightSlider} ref={rightDot} type="range" onInput={(e) => changeValue(e, 'right')} className={s.rangeBlockInputRight} />
             </div>
             <div className={s.rangeBlockNumbers}>
                 <div className={s.rangeBlockNumbersHigh}>
-                    <span className={s.rangeBlockNumbersHighText}>От:</span>
-                    <input type="number" name="" id="" className={s.rangeBlockNumbersHighInput} />
+                    <span className={s.rangeBlockNumbersLowText}>От:</span>
+                    <input type="number" ref={maxValue} value={leftSlider} className={s.rangeBlockNumbersHighInput} />
                 </div>
                 <div className={s.rangeBlockNumbersLow}>
-                    <span className={s.rangeBlockNumbersLowText}>До:</span>
-                    <input type="number" name="" id="" className={s.rangeBlockNumbersLowInput} />
+                    <span className={s.rangeBlockNumbersHighText}>До:</span>
+                    <input type="number" ref={minValue} value={rightSlider} className={s.rangeBlockNumbersLowInput} />
                 </div>
             </div>
         </div>
