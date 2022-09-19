be05253843c7d7e5b6c40c19edf6bf81b5fae4d4
TheSnegok <lord180499@gmail.com>
2022-09-19 20:20:15 +0300

    changed priceDivision margine

--- src/Components/Templates/PriceDivision/PriceDivision.tsx
+++ src/Components/Templates/PriceDivision/PriceDivision.tsx
@@ -7,7 +7,7 @@ export const PriceDivision = ({ price, color }: { price: number[], color?: boole
             {
                 price.length === 1 ? (
                     <div className={s.price}>
-                        <span className={color ? s.priceBlack : s.priceWhite}>{numberWithSpaces(price[0], true)}</span>
+                        <span className={color ? s.priceBlackOnce : s.priceWhiteOnce}>{numberWithSpaces(price[0], true)}</span>
                     </div>
                 ) : (
                     <div className={s.price} data-diuscount={numberWithSpaces(price[1], true)}>
