9c7159063683e9a97d054d0e8b1c6b5e17390cb5
TheSnegok <lord180499@gmail.com>
2022-09-21 16:05:57 +0300

    maked responsive tags for <800px

--- src/Components/SearchPage/SearchPage.tsx
+++ src/Components/SearchPage/SearchPage.tsx
@@ -2,7 +2,7 @@ import { FC, useEffect, useState } from "react";
 import { useNavigate } from "react-router";
 import { IDataTemplate, useGlobalContext } from "../Context/Context"
 import { PriceDivision } from "../Templates/PriceDivision/PriceDivision";
-import { RangeDoubleSlider } from "./RangeDoubleSlider/RangeDoubleSlider";
+import { RangeDoubleSlider } from "../Templates/RangeDoubleSlider/RangeDoubleSlider";
 import s from "./SearchPage.module.sass"
 
 export const SearchPage: FC = () => {
@@ -52,7 +52,7 @@ export const SearchPage: FC = () => {
                 sort: 2
             });
             setMatched([...findInput.matchFound.sort((first, second) => second.price[0] - first.price[0])]);
-        }
+        }// eslint-disable-next-line
     }, [findInput.matchFound, selectValue])
 
     return (
