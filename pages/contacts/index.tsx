83720e68ff46ae71ebba6c54da2683406581f935
TheSnegok <lord180499@gmail.com>
2022-10-13 18:19:39 +0300

    stylization contactsPage

--- pages/contacts/index.tsx
+++ pages/contacts/index.tsx
@@ -11,21 +11,28 @@ const ContactsPage = () => {
     return (
         <section className={s.contacts}>
             <h3 className={s.contactsHeader}>Контакты</h3>
-            <p>Зв'язатися за нами можливо за допомогою телефона:</p>
-            <div className={s.contactsNumber}>
-                <a href="tel:+380950183210">+38 095 018-32-10</a>
-            </div>
-            <p>Або в нашому центрі, виберіть найближче місце до вас:</p>
-            <div className={s.contactsWrapper}>
-                <div className={s.contactsWrapperMenu}>
-                    {data.regionsList.map(region => (
-                        <div className={s.contactsWrapperMenuRegion}>
-                            <span>{region}</span>
-                        </div>
-                    ))}
+            <div className={s.contactsPhone}>
+                <p>Зв'язатися за нами можливо за допомогою телефона:</p>
+                <div className={s.contactsPhoneNumber}>
+                    <a href="tel:+380950183210">+38 095 018-32-10</a>
                 </div>
-                <div className={s.contactsWrapperMap}>
-                    <iframe src={`https://maps.google.com/maps?q=${coord}&z=15&output=embed`} width="600" height="450" style={{ border: 0 }} loading="lazy" referrerPolicy="no-referrer-when-downgrade" />
+            </div>
+            <div className={s.contactsOr}>
+                <span>або</span>
+            </div>
+            <div className={s.contactsLocation}>
+                <p>Або в нашому центрі, виберіть найближче місце до вас:</p>
+                <div className={s.contactsLocationMenu}>
+                    <div className={s.contactsLocationMenuList}>
+                        {data.regionsList.map(region => (
+                            <div className={s.contactsLocationMenuListRegion}>
+                                <span>{region}</span>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+                <div className={s.contactsLocationMap}>
+                    <iframe src={`https://maps.google.com/maps?q=${coord}&z=15&output=embed`} loading="lazy" referrerPolicy="no-referrer-when-downgrade" />
                 </div>
             </div>
         </section>
