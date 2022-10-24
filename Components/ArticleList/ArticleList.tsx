3348dcc59fd26e281554b8966bc4de2e6898dcd2
TheSnegok <lord180499@gmail.com>
2022-10-24 22:04:41 +0300

    created styles for changes template in articles

--- Components/ArticleList/ArticleList.tsx
+++ Components/ArticleList/ArticleList.tsx
@@ -1,21 +1,32 @@
 import Image, { StaticImageData } from "next/image";
+import Link from "next/link";
 import s from "../../Styles/components/ArticleList.module.sass";
+import { IArticle } from "../Context/Context";
+import { articleText } from "../../pages/api/api";
 
 interface IArticleList {
     image: StaticImageData;
     header: string;
+    setArticle: (c: IArticle) => void;
 }
 
-const ArticleList = ({ image, header }: IArticleList) => {
+const ArticleList = ({ image, header, setArticle }: IArticleList) => {
+
+    const pushArticle = () => {
+        setArticle({ articleHeader: header, articleText: articleText[articleText.findIndex(element => element.header === header)].text });
+    }
+
     return (
-        <div className={s.item}>
-            <div className={s.itemImg}>
-                <Image src={image} alt={header} layout="fill" />
-            </div>
-            <h4 className={s.itemHeader}>
-                {header}
-            </h4>
-        </div>
+        <Link href="/article">
+            <a onClick={pushArticle} className={s.item}>
+                <div className={s.itemImg}>
+                    <Image src={image} alt={header} layout="fill" />
+                </div>
+                <h4 className={s.itemHeader}>
+                    {header}
+                </h4>
+            </a>
+        </Link>
     )
 }
 
--- pages/articles/index.tsx
+++ pages/articles/index.tsx
@@ -5,39 +5,13 @@ import s from "../../Styles/pages/Articles.module.sass";
 
 const Articles = () => {
 
-	const { data } = useGlobalContext();
+	const { data, setArticle } = useGlobalContext();
 
 	return (
 		<section className={s.articles}>
 			<h1 className={s.articlesHeader}>Статьи</h1>
 			<div className={s.articlesWrapper}>
 				<div className={s.articlesWrapperOptions}>
-					<div className={s.articlesWrapperOptionsSort}>
-						<div className={s.articlesWrapperOptionsSelected}>
-							<div className={s.articlesWrapperOptionsSelectedImg}>
-								<img src="" alt="" />
-								<span></span>
-							</div>
-						</div>
-						<div className={s.articlesWrapperOptionsList}>
-							<div className={s.articlesWrapperOptionsListDateNew}>
-								<img src="" alt="" />
-								<span></span>
-							</div>
-							<div className={s.articlesWrapperOptionsListDateLast}>
-								<img src="" alt="" />
-								<span></span>
-							</div>
-							<div className={s.articlesWrapperOptionsListMoreRead}>
-								<img src="" alt="" />
-								<span></span>
-							</div>
-							<div className={s.articlesWrapperOptionsListLessRead}>
-								<img src="" alt="" />
-								<span></span>
-							</div>
-						</div>
-					</div>
 					<div className={s.articlesWrapperOptionsTemplate}>
 						<div className={s.articlesWrapperOptionsTemplateRow}>
 							<div className={s.articlesWrapperOptionsTemplateRowLine}></div>
@@ -45,14 +19,20 @@ const Articles = () => {
 							<div className={s.articlesWrapperOptionsTemplateRowLine}></div>
 						</div>
 						<div className={s.articlesWrapperOptionsTemplateBox}>
-							<div className={s.articlesWrapperOptionsTemplateBoxLine}></div>
-							<div className={s.articlesWrapperOptionsTemplateBoxLine}></div>
-							<div className={s.articlesWrapperOptionsTemplateBoxLine}></div>
+							<div className={s.articlesWrapperOptionsTemplateBoxItem}></div>
+							<div className={s.articlesWrapperOptionsTemplateBoxItem}></div>
+							<div className={s.articlesWrapperOptionsTemplateBoxItem}></div>
+							<div className={s.articlesWrapperOptionsTemplateBoxItem}></div>
+							<div className={s.articlesWrapperOptionsTemplateBoxItem}></div>
+							<div className={s.articlesWrapperOptionsTemplateBoxItem}></div>
+							<div className={s.articlesWrapperOptionsTemplateBoxItem}></div>
+							<div className={s.articlesWrapperOptionsTemplateBoxItem}></div>
+							<div className={s.articlesWrapperOptionsTemplateBoxItem}></div>
 						</div>
 					</div>
 				</div>
 				<div className={s.articlesWrapperItems}>
-					{data.articleList.map((item, index) => <ArticleList key={index} image={item[0] as StaticImageData} header={item[1] as string} />)}
+					{data.articleList.map((item, index) => <ArticleList key={index} image={item[0] as StaticImageData} header={item[1] as string} setArticle={setArticle} />)}
 				</div>
 			</div>
 		</section>
