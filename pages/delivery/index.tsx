d2c9a97a4e03e235b9e4c097c2cc75e9a3fb97dd
TheSnegok <lord180499@gmail.com>
2022-10-28 20:34:06 +0300

    created delivery page

---
+++ pages/delivery/index.tsx
@@ -0,0 +1,11 @@
+import s from '../../Styles/pages/Delivery.module.sass';
+
+const Delivery = () => {
+    return (
+        <section className={s.delivery}>
+            <h2 className={s.deliveryHeader}>Доставка</h2>
+        </section>
+    )
+}
+
+export default Delivery;
