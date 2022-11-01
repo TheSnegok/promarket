16b40ca67cc95e111773c3e7ecbbb9d19f8fb657
TheSnegok <lord180499@gmail.com>
2022-11-01 17:29:58 +0200

    created some styles and add Banks in payment page

--- pages/payment/index.tsx
+++ pages/payment/index.tsx
@@ -6,30 +6,32 @@ const Payment = () => {
         <section className={s.payment}>
             <h2 className={s.paymentHeader}>Оплата</h2>
             <div className={s.paymentWrapper}>
-                <h4 className={s.paymentWrapperHeader}>Есть возможность оплаты двумя вариантами:</h4>
+                <h4 className={s.paymentWrapperHeader}>Как я могу росплатится?</h4>
                 <div className={s.paymentWrapperCash}>
-                    <span>Вы можете заплатить наличными при доставке.</span>
+                    <span>Вы можете заплатить наличными или по карте при доставке.</span>
                     <div className={s.paymentWrapperCashImg}>
                         <Image src="" alt="cash" />
                     </div>
                 </div>
-                <div>Или</div>
+                <div className={s.paymentWrapperAnd}>Или</div>
                 <div className={s.paymentWrapperCard}>
                     <span>Вы можете заплатить при оформлении заказа любыми картами из ниже перечисленных.</span>
                     <div className={s.paymentWrapperCardImg}>
                         <Image src="" alt="card" />
                     </div>
-                    Мы поддерживаем такие платёжные системы:
-                    <div className={s.paymentWrapperCardNames}>
-                        <div className={s.paymentWrapperCardNamesVisa}>
-                            <Image src="" alt="Visa" />
-                        </div>
-                        <div className={s.paymentWrapperCardNamesPaypal}>
-                            <Image src="" alt="Paypal" />
-                        </div>
-                        <div className={s.paymentWrapperCardNamesMastercard}>
-                            <Image src="" alt="Mastercard" />
-                        </div>
+                </div>
+            </div>
+            <div className={s.paymentBanks}>
+                <h5 className={s.paymentBanks}>Мы поддерживаем такие банковские системы как:</h5>
+                <div className={s.paymentBanksWrapper}>
+                    <div className={s.paymentBanksWrapperImg}>
+                        <Image src="" alt="Visa" />
+                    </div>
+                    <div className={s.paymentrBanksWrapperImg}>
+                        <Image src="" alt="Paypal" />
+                    </div>
+                    <div className={s.paymentBanksWrapperImg}>
+                        <Image src="" alt="Mastercard" />
                     </div>
                 </div>
             </div>
