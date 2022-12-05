6574a4440a7dda5782cbf492ce1e454b401eaed6
TheSnegok <lord180499@gmail.com>
2022-12-05 20:05:51 +0200

    styled  registration

--- pages/registration/index.tsx
+++ pages/registration/index.tsx
@@ -2,20 +2,36 @@ import TypicalPage from "~/../Components/Templates/TypicalPage/TypicalPage"
 import s from "../../Styles/pages/Registration.module.sass";
 
 const Registration = () => {
-    return(
+    return (
         <TypicalPage header="Регистрация">
             <div className={s.registration}>
-                <form action="POST">
-                    <label htmlFor="name">Name</label>
-                    <input type="text" name="name" />
-                    <label htmlFor="secondName">Second Name</label>
-                    <input type="text" name="secondName" />
-                    <label htmlFor="email">E-mail</label>
-                    <input type="e-mail" name="email" />
-                    <label htmlFor="pass">Password</label>
-                    <input type="pass" name="pass" />
-                    <button>Registration</button>
-                </form>
+                <fieldset>
+                    <form action="POST">
+                        <div className={s.registrationField}>
+                            <label htmlFor="name">Name:</label>
+                            <input type="text" name="name" />
+                        </div>
+                        <div className={s.registrationField}>
+                            <label htmlFor="secondName">Second Name:</label>
+                            <input type="text" name="secondName" />
+                        </div>
+                        <div className={s.registrationField}>
+                            <label htmlFor="email">E-mail:</label>
+                            <input type="e-mail" name="email" />
+                        </div>
+                        <div className={s.registrationField}>
+                            <label htmlFor="pass">Password:</label>
+                            <input type="pass" name="pass" />
+                        </div>
+                        <div className={s.registrationButton}>
+                            <button>
+                                <span>
+                                    Зарегистрироваться
+                                </span>
+                            </button>
+                        </div>
+                    </form>
+                </fieldset>
             </div>
         </TypicalPage>
     )
