2887a854866847adfe9cb5b04111df4fb343ce0e
TheSnegok <lord180499@gmail.com>
2022-12-06 23:22:28 +0200

    added preventdefault for button

--- pages/registration/index.tsx
+++ pages/registration/index.tsx
@@ -1,12 +1,33 @@
+import { useRouter } from "next/router";
+import { useCallback, useEffect, useRef } from "react";
 import TypicalPage from "~/../Components/Templates/TypicalPage/TypicalPage"
 import s from "../../Styles/pages/Registration.module.sass";
 
 const Registration = () => {
+
+    const button = useRef(null);
+    const form = useRef(null);
+    const router = useRouter();
+
+    const buttonEvent = useCallback(e => {
+        e.preventdefault();
+        router.push('/login');
+    }, []);
+
+    useEffect(() => {
+        if (button !== null) {
+            button.current.addEventListener("onclick", buttonEvent);
+        }
+        return () => {
+            button.current.removeEventListener("onclick", buttonEvent);
+        }
+    });
+
     return (
         <TypicalPage header="Регистрация">
             <div className={s.registration}>
                 <fieldset>
-                    <form action="POST">
+                    <form action="POST" ref={form}>
                         <div className={s.registrationField}>
                             <label htmlFor="name">Name:</label>
                             <input type="text" name="name" />
@@ -24,7 +45,7 @@ const Registration = () => {
                             <input type="pass" name="pass" />
                         </div>
                         <div className={s.registrationButton}>
-                            <button>
+                            <button type="submit" ref={button}>
                                 <span>
                                     Зарегистрироваться
                                 </span>
